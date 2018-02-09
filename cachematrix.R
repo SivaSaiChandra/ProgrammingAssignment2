aac181d0750d911ce1b2257e2f20113d9fdf195f
SivaSaiChandra <sivasaichandra@gmail.com>
2018-02-09 21:01:44 +0530

    First Commiting
    Commiting the code

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Cousera Data Science Specialization by John Hopkins University
+## R Programming course - Programming Assognment 2
+## Invert a marix and cache its result
+## so that subsequent requests for matrix inversion
+## retrieve from cache rather than redoing the inverse calculation
+## Refer for understanding of the code(similar code)- https://github.com/lgreski/datasciencectacontent/blob/master/markdown/rprog-breakingDownMakeVector.md
 
-## Write a short comment describing this function
+## Creates a cache for a matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        i <- NULL
+        set <- function(y)      {
+                x <<- y
+                i <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) i <<- inverse
+        getinverse <- function() i
+        list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Returns cached inverse for old matrix or Calculates the inverse for the new matrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        i <- x$getinverse()
+        if(!is.null(i)) {
+                message("getting cached data")
+                return(i)
+        }
+        data <- x$get()
+        i <- solve(data, ...)
+        x$setinverse(i)
+        i
 }
